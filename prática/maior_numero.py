87d705795423d23e4d736b5be572ac6bdbb256e9
Gabi <143514875+Gaabi1@users.noreply.github.com>
2025-03-26 14:59:32 +0000

    maior numero

---
+++ prática/maior_numero.py
@@ -0,0 +1,18 @@
+#Escreva uma função que busca de forma iterativa o maior elemento em uma lista de números inteiros.
+
+def maior_numero(lista):
+
+    #Se a lista tiver um elemento 
+    if len(lista)<=1:
+        return lista[0]
+    
+    maior_elemento= lista[0]
+    for numero in lista[1:]:
+        if numero> maior_elemento:
+            maior_elemento=numero
+    return maior_elemento
+
+lista= input("Entre com os números: ")
+maior_elemento= maior_numero(lista)
+print(f"O maior número é: {maior_elemento}")
+
